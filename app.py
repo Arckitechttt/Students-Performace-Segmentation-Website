0f2b99dc5f5830c42f83dd5717e282a4fc38ed26
Lucas Traumen <73390184+Arckitecht@users.noreply.github.com>
2022-11-26 16:14:43 +0700

    Update #2

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import crypt.methods
+import imp
+from pyexpat import features
+
 from flask import Flask, request, render_template
+import numpy as np
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
 
 @app.route('/',methods=["GET","POST"])
 
@@ -8,9 +15,12 @@ def home():
     AuthorName = "Arckitecht"
     return render_template("home.html", user=AuthorName)
 
-@app.route('/cryo-model',methods=["GET","POST"])
-def cryoModel():
-    return render_template("cryo-model.html")
+@app.route('/cryo-model',methods=["POST"])
+def cryomodel():
+    float_features = [float(x) for x in request.form.values()]
+    feature = [np.array(float_features)]
+    prediction = model.predict(feature)
+    return render_template("cryo-model.html", prediction_text="{}".format(prediction))
 
 if __name__ == '__main__':
     app.run(debug=True)
